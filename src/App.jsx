76e13c0e7dd24729dfc10f9798377b69962c0964
Filipe Oliveira <filipeoliveira0426@gmail.com>
2023-08-21 19:10:02 -0300

    criação da estrutura base

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+	return (
+		<>
+			<div className=' min-h-screen text-zinc-900 bg-gradient-to-b from-sky-400 to-sky-200 p-8'>
+				<div className='bg-white h-5/6 w-full mx-auto rounded-xl min-h-[200px] shadow-lg border border-black/20 overflow-hidden grid grid-cols-[16rem_1fr]'>
+					<aside className='bg-zinc-50'></aside>
+					<main className='bg-white p-5'></main>
+				</div>
+			</div>
+		</>
+	);
 }
-
-export default App
