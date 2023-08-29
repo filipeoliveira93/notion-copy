f276b186b80d0a6aaa2a8795104aeee02ead84a1
Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2023-08-28 23:52:26 -0300

    added the rect-icons to the bubble-menu

--- src/components/Tiptap.jsx
+++ src/components/Tiptap.jsx
@@ -1,7 +1,14 @@
 // src/components/Tiptap.jsx
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
 import { lowlight } from "lowlight/lib/core";
 import StarterKit from "@tiptap/starter-kit";
+
+import {
+	RxFontBold,
+	RxFontItalic,
+	RxStrikethrough,
+	RxCode,
+} from "react-icons/rx";
 
 // highlight syntax
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
@@ -14,7 +21,8 @@ import html from "highlight.js/lib/languages/xml";
 import { initialContent } from "./initialContent";
 
 import "highlight.js/styles/tokyo-night-dark.css";
-
+import { RxScissors } from "react-icons/rx/index.esm";
+// Register languages used for syntax highlighting
 lowlight.registerLanguage("html", html);
 lowlight.registerLanguage("css", css);
 lowlight.registerLanguage("js", js);
@@ -40,7 +48,29 @@ const Tiptap = () => {
 		},
 	});
 
-	return <EditorContent editor={editor} className='p-20 w-full' />;
+	return (
+		<>
+			<EditorContent editor={editor} className='p-20 w-full' />;
+			{editor && (
+				<BubbleMenu
+					className='bg-zinc-700 text-zinc-100 shadow-lg border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600'
+					editor={editor}>
+					<button className='p-2 text-zinc-200 hover:text-zinc-50 hover:bg-zinc-600 text-sm flex items-center  font-medium leading-none ease duration-150'>
+						<RxFontBold className='w-4 h-4' />
+					</button>
+					<button className='p-2 text-zinc-200 hover:text-zinc-50 hover:bg-zinc-600 text-sm flex items-center  font-medium leading-none ease duration-150'>
+						<RxFontItalic className='w-4 h-4' />
+					</button>
+					<button className='p-2 text-zinc-200 hover:text-zinc-50 hover:bg-zinc-600 text-sm flex items-center  font-medium leading-none ease duration-150'>
+						<RxScissors className='w-4 h-4' />
+					</button>
+					<button className='p-2 text-zinc-200 hover:text-zinc-50 hover:bg-zinc-600 text-sm flex items-center  font-medium leading-none ease duration-150'>
+						<RxCode className='w-4 h-4' />
+					</button>
+				</BubbleMenu>
+			)}
+		</>
+	);
 };
 
 export default Tiptap;
